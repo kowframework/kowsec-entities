27f79334caf4707cac79c22adb8c7803e6eb1dc7
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2010-12-08 21:30:23 -0200

    fixing up copyright notice

--- src/kow_sec_entities.ads
+++ src/kow_sec_entities.ads
@@ -1,5 +1,41 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                       KOW Framework :: Security                          --
+--                                                                          --
+--                              KOW Framework                               --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--               Copyright (C) 2007-2011, KOW Framework Project             --
+--                                                                          --
+--                                                                          --
+-- KOWSec is free software; you can redistribute it  and/or modify it under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion. KOWSec is distributed in the hope that it will be useful, but WITH---
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License distributed with KOWSec; see file COPYING.  If not, write --
+-- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
+-- MA 02111-1307, USA.                                                      --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+------------------------------------------------------------------------------
 
 
+------------------------------------------------------------------------------
+-- Main package for KOW Sec Entities                                        --
+--                                                                          --
+-- This package implements an Authorization Manager using KOW_Ent as the    --
+-- backend for login data                                                   --
+------------------------------------------------------------------------------
 
 --------------
 -- Ada 2005 --
@@ -7,9 +43,9 @@
 with Ada.Strings.Unbounded;		use Ada.Strings.Unbounded;
 
 
----------------
--- Ada Works --
----------------
+-------------------
+-- KOW Framework --
+-------------------
 with KOW_Ent;
 with KOW_Ent.Query_Builders;
 with KOW_Sec;				use KOW_Sec;
